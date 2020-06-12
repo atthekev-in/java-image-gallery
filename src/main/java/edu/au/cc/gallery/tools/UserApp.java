fd8d30b324afbcd5fc9bedfeec4997dc4fc75842
atthekev-in <kzw0068@auburn.edu>
2020-06-12 21:06:10 +0000

    added redirects

--- src/main/java/edu/au/cc/gallery/tools/UserApp.java
+++ src/main/java/edu/au/cc/gallery/tools/UserApp.java
@@ -1,4 +1,5 @@
 package edu.au.cc.gallery.tools;
+
 import edu.au.cc.gallery.DB;
 
 import java.sql.SQLException;
@@ -16,7 +17,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class UserApp {
     DB db = new DB();
-    
+
     public void addRoutes() {
         get("/admin", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
@@ -28,7 +29,7 @@ public class UserApp {
             Map<String, Object> model = new HashMap<>();
             return new HandlebarsTemplateEngine().render(new ModelAndView(model, "addUser.hbs"));
         });
-        
+
         get("/admin/editUser/:user", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
 
@@ -36,25 +37,27 @@ public class UserApp {
             model.put("username", user);
             return new HandlebarsTemplateEngine().render(new ModelAndView(model, "editUser.hbs"));
         });
-        post("/admin/addUser", (req, res) -> createUser(req, res));
-        post("/admin/editUser", (req, res) -> editUser(req, res));
-        post("/admin/deleteUser", (req, res) -> deleteUser(req,res));
-    }
-    
-   
+        post("/admin/addUser", (req, res) -> {
+            String[] user = { req.queryParams("username"), req.queryParams("password"), req.queryParams("full_name") };
+            db.createUser(user);
+            res.redirect("/admin");
+            return "added user";
 
-    public String createUser(Request req, Response res) throws SQLException {
-        String[] user = {req.queryParams("username"), req.queryParams("password"), req.queryParams("full_name")};
-        db.createUser(user);
-        return "user added";
+        });
+        post("/admin/editUser", (req, res) -> {
+            db.editUser(req.queryParams("username"), req.queryParams("password"), req.queryParams("full_name"));
+            res.redirect("/admin");
+            return "modified user";
+
+        });
+        post("/admin/deleteUser", (req, res) -> {
+            db.deleteUser(req.queryParams("username"));
+            res.redirect("/admin");
+            return "deleted user";
+
+        });
+
     }
-    public String editUser(Request req, Response res) throws SQLException {
-	System.out.println(req.queryParams("username") + req.queryParams("password") + req.queryParams("full_name"));
-        db.editUser(req.queryParams("username"), req.queryParams("password"), req.queryParams("full_name"));
-        return "user edited";
-    }
-    public String deleteUser(Request req, Response res) throws SQLException {
-        db.deleteUser(req.queryParams("username"));
-        return "user deleted";
-    }
+
 }
+
